a619f649cd73af96b12b4ec8740a205996b4581f
pryz25 <pryzanych@gmail.com>
2019-03-01 17:34:39 -0800

    Started creating chat client

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -3,9 +3,16 @@ var MessagesView = {
   $chats: $('#chats'),
 
   initialize: function() {
+    Parse.readAll((data) => { // call parse to pull messages
+      for (var key in data) { // for the single key in data
+        Messages.storage = data[key]; // storage is server message array
+      }
+    });
   },
 
-  render: function() {
+  renderMessage: function(message) {
+    var h = MessageView.render(message);
+    $chats.append(h);
   }
 
 };
